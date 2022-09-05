992597a8071812b0d233dc074f1133c6e6b4496d
oscargim7 <oscarromero1601@gmail.com>
2022-09-04 19:14:01 -0500

    Intro al programa

--- Figure.cpp
+++ Figure.cpp
@@ -1,3 +1,12 @@
+/*
+    Romero Escamilla Oscar Eduardo
+    22110112
+    2P
+    Programación Orientada en Objetos
+    Descripción de programa:
+        - Desarrollo de 3 figuras con el método draw en c++
+*/
+
 #include <iostream>
 #include "InputManager.h"
 #include "Figure.h"
